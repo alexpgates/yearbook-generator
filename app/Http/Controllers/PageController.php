72d54db8b47479e73229ae2322b47a6ef5d358bc
Alex <alex@whatcheer.com>
2019-05-07 17:29:37 -0500

    i dunno, more fixes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,13 +11,13 @@ class PageController extends Controller
     public function show($teacher){
         $students = Student::where('teacher', $teacher)->orderBy('last_name', 'ASC')->get();
         $teacher = StaffMember::where('last_name', $teacher)->first();
-        $support = StaffMember::where('teacher_room', $teacher)->where('last_name', '!='. $teacher)->get();
+        $support = StaffMember::where('teacher_room', $teacher->last_name)->where('last_name', '!=', $teacher->last_name)->get();
         return view('classroom.show', compact('students', 'teacher', 'support'));
     }
 
     public function staff(){
         $principal = StaffMember::where('title', 'principal')->first();
-        $staff = StaffMember::where('teacher_room', 'staff')->where('title', '!=', 'principal')->orWhereNull('title')->get();
+        $staff = StaffMember::where('teacher_room', 'staff')->where('title', '!=', 'principal')->orderBy('last_name', 'ASC')->get();
         return view('staff.show', compact('principal', 'staff'));
     }
 }
